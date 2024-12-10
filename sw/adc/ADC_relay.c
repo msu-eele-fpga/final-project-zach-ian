f2ec1a7f485448fa9725a0236d3ea5ae5a4ce1e1
Ian Crittenden <iancrittenden@rocketmail.com>
2024-12-09 22:31:50 -0700

    ADC_INTERFACE: adc sends valuse correctly to rbgled and hardware respondes

--- sw/adc/ADC_relay.c
+++ sw/adc/ADC_relay.c
@@ -9,6 +9,9 @@ int main(void){
 		FILE* f1 = fopen ("/sys/devices/platform/ff200000.adc/ch0_raw", "r");
 		FILE* f2 = fopen ("/sys/devices/platform/ff200000.adc/ch1_raw", "r");
 		FILE* f3 = fopen ("/sys/devices/platform/ff200000.adc/ch2_raw", "r");
+		FILE* f4 = fopen ("/sys/devices/platform/ff200020.rgbled/red_duty", "w");
+		FILE* f5 = fopen ("/sys/devices/platform/ff200020.rgbled/green_duty", "w");
+		FILE* f6 = fopen ("/sys/devices/platform/ff200020.rgbled/blue_duty", "w");
 		int a = 0;
 		int b = 0;
 		int c = 0;
@@ -18,10 +21,17 @@ int main(void){
 		a = a >> 1;
 		b = b >> 1;
 		c = c >> 1;
-		printf("%d %d %d\n", a, b, c);
+		//printf("%d %d %d\n", a, b, c);
+		
+		fprintf(f4, "%d",a);
+		fprintf(f5, "%d",b);
+		fprintf(f6, "%d",c);
 
 		fclose(f1);
 		fclose(f2);
 		fclose(f3);
+		fclose(f4);
+		fclose(f5);
+		fclose(f6);
 	}
 }
